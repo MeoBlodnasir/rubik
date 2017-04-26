621de1003fb1085dc99f67aaa036a3fa62d14584
gonzo <blodnasir@gmail.com>
2017-04-26 17:44:17 +0200

    m

--- main.py
+++ main.py
@@ -3,13 +3,6 @@ from cube import Cube
 if __name__ == "__main__":
     cube = Cube()
     print(cube)
-    #print("Rotate Up")
-    #cube.rotate_up()
-    #print(cube)
-    #print("Rotate U")
-    #cube.rotate("U")
-    #print(cube)
-    #cube.solve()
-    cube.randomize()
+    cube.rotate("R")
     print(cube)
-   # cube.solve()
+
