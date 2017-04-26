a09faf12418478f5e38867406efdcc08cafa1e10
abombard <arthbab@gmail.com>
2017-04-26 15:13:01 +0200

    rotateCube + new display

--- cube.py
+++ cube.py
@@ -1,3 +1,5 @@
+import re
+
 class Square(object):
     def __init__(self, color):
         self.color = color
@@ -7,7 +9,7 @@ class Square(object):
 class Face(object):
     def __init__(self, color):
         self.squares = [color, color, color, color, color, color, color, color, color]
-        self.up_row = None
+        self.top_row = None
         self.bot_row = None
         self.left_row = None
         self.right_row = None
@@ -19,14 +21,14 @@ class Face(object):
     def left_border(self):
         return [self.squares[0], self.squares[3], self.squares[6]]
     def right_border(self):
-
         return [self.squares[2], self.squares[5], self.squares[8]]
-    def link(self, up_row, bot_row, left_row, right_row):
-        self.up_row = up_row
+
+    def link(self, top_row, bot_row, left_row, right_row):
+        self.top_row = top_row
         self.bot_row = bot_row
         self.left_row = left_row
-
         self.right_row = right_row
+
     def __str__(self):
         return ("{0} {1} {2}\n{3} {4} {5}\n{6} {7} {8}\n".format(self.squares[0],
             self.squares[1],
@@ -83,5 +85,57 @@ class Cube(object):
             self.left.bot_border()
         )
 
+    def rotate_up(self):
+        switch = self.front
+        self.front = self.bot
+        self.bot = self.back
+        self.back = self.top
+        self.top = switch
+
+    def rotate_down(self):
+        switch = self.front
+        self.front = self.top
+        self.top = self.back
+        self.back = self.bot
+        self.bot = switch
+
+    def rotate_left(self):
+        switch = self.front
+        self.front = self.right
+        self.right = self.back
+        self.back = self.left
+        self.left = switch
+
+    def rotate_right(self):
+        switch = self.front
+        self.front = self.left
+        self.left = self.back
+        self.back = self.right
+        self.right = switch
+
     def __str__(self):
-        return "FRONT\n{0}\nLEFT\n{1}\nRIGHT\n{2}\nTOP\n{3}\nBOTTOM\n{4}\nBACK\n{5}\n___________________________".format(self.front, self.left, self.right, self.top, self.bot, self.back)
+
+        def line(c, n):
+            return c[n*5:n*5+5]
+
+        empty = "     "
+        front = re.sub('\n', '', self.front.__str__())
+        back = re.sub('\n', '', self.back.__str__())
+        left = re.sub('\n', '', self.back.__str__())
+        right = re.sub('\n', '', self.right.__str__())
+        top = re.sub('\n', '', self.top.__str__())
+        bot = re.sub('\n', '', self.bot.__str__())
+        string = ("Cube:\n" +
+            empty         + " " + line(top, 0)   + " " +  empty          + "\n" +
+            empty         + " " + line(top, 1)   + " " +  empty          + "\n" +
+            empty         + " " + line(top, 2)   + " " +  empty          + "\n" +
+            line(left, 0) + " " + line(front, 0) + " " +  line(right, 0) + "\n" +
+            line(left, 1) + " " + line(front, 1) + " " +  line(right, 1) + "\n" +
+            line(left, 2) + " " + line(front, 2) + " " +  line(right, 2) + "\n" +
+            empty         + " " + line(bot, 0)   + " " +  empty          + "\n" +
+            empty         + " " + line(bot, 1)   + " " +  empty          + "\n" +
+            empty         + " " + line(bot, 2)   + " " +  empty          + "\n" +
+            empty         + " " + line(back, 0)  + " " +  empty          + "\n" +
+            empty         + " " + line(back, 1)  + " " +  empty          + "\n" +
+            empty         + " " + line(back, 2)  + " " +  empty          + "\n")
+        return string
