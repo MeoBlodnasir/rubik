1611e5c57f95fccbe5a6abe0281d759457ff07e0
gonzo <blodnasir@gmail.com>
2017-04-26 13:02:20 +0200

    no more square, init easier

--- cube.py
+++ cube.py
@@ -1,102 +1,38 @@
 import copy
-class Square:
-    def __init__(self):
-        self.colour = "X"
-    def __str__(self):
-        return self.colour
 
 class Row:
-    def __init__(self):
-        self.left_square = Square()
-        self.middle_square = Square()
-        self.right_square = Square()
+    def __init__(self, color):
+        self.left_square = color
+        self.middle_square = color
+        self.right_square = color
     def __str__(self):
         return ("{0} {1} {2}".format(self.left_square, self.middle_square, self.right_square))
 
 class Face:
-    def __init__(self):
-        self.top_row = Row()
-        self.middle_row = Row()
-        self.bottom_row = Row()
+    def __init__(self, color):
+        self.top_row = Row(color)
+        self.middle_row = Row(color)
+        self.bottom_row = Row(color)
     def __str__(self):
         return ("{0}\n{1}\n{2}".format(self.top_row, self.middle_row, self.bottom_row))
 
 class Cube:
     def __init__(self):
-        self.front_face = Face()
-        self.left_face = Face()
-        self.right_face = Face()
-        self.top_face = Face()
-        self.bottom_face = Face()
-        self.back_face = Face()
-        self.front_face.top_row.left_square.colour = "G"
-        self.front_face.top_row.middle_square.colour = "G"
-        self.front_face.top_row.right_square.colour = "G"
-        self.front_face.middle_row.left_square.colour = "G"
-        self.front_face.middle_row.middle_square.colour = "G"
-        self.front_face.middle_row.right_square.colour = "G"
-        self.front_face.bottom_row.left_square.colour = "G"
-        self.front_face.bottom_row.middle_square.colour = "G"
-        self.front_face.bottom_row.right_square.colour = "G"
-
-        self.left_face.top_row.left_square.colour = "R"
-        self.left_face.top_row.middle_square.colour = "R"
-        self.left_face.top_row.right_square.colour = "R"
-        self.left_face.middle_row.left_square.colour = "R"
-        self.left_face.middle_row.middle_square.colour = "R"
-        self.left_face.middle_row.right_square.colour = "R"
-        self.left_face.bottom_row.left_square.colour = "R"
-        self.left_face.bottom_row.middle_square.colour = "R"
-        self.left_face.bottom_row.right_square.colour = "R"
-
-        self.right_face.top_row.left_square.colour = "O"
-        self.right_face.top_row.middle_square.colour = "O"
-        self.right_face.top_row.right_square.colour = "O"
-        self.right_face.middle_row.left_square.colour = "O"
-        self.right_face.middle_row.middle_square.colour = "O"
-        self.right_face.middle_row.right_square.colour = "O"
-        self.right_face.bottom_row.left_square.colour = "O"
-        self.right_face.bottom_row.middle_square.colour = "O"
-        self.right_face.bottom_row.right_square.colour = "O"
-
-        self.top_face.top_row.left_square.colour = "Y"
-        self.top_face.top_row.middle_square.colour = "Y"
-        self.top_face.top_row.right_square.colour = "Y"
-        self.top_face.middle_row.left_square.colour = "Y"
-        self.top_face.middle_row.middle_square.colour = "Y"
-        self.top_face.middle_row.right_square.colour = "Y"
-        self.top_face.bottom_row.left_square.colour = "Y"
-        self.top_face.bottom_row.middle_square.colour = "Y"
-        self.top_face.bottom_row.right_square.colour = "Y"
-
-        self.bottom_face.top_row.left_square.colour = "W"
-        self.bottom_face.top_row.middle_square.colour = "W"
-        self.bottom_face.top_row.right_square.colour = "W"
-        self.bottom_face.middle_row.left_square.colour = "W"
-        self.bottom_face.middle_row.middle_square.colour = "W"
-        self.bottom_face.middle_row.right_square.colour = "W"
-        self.bottom_face.bottom_row.left_square.colour = "W"
-        self.bottom_face.bottom_row.middle_square.colour = "W"
-        self.bottom_face.bottom_row.right_square.colour = "W"
-
-        self.back_face.top_row.left_square.colour = "B"
-        self.back_face.top_row.middle_square.colour = "B"
-        self.back_face.top_row.right_square.colour = "B"
-        self.back_face.middle_row.left_square.colour = "B"
-        self.back_face.middle_row.middle_square.colour = "B"
-        self.back_face.middle_row.right_square.colour = "B"
-        self.back_face.bottom_row.left_square.colour = "B"
-        self.back_face.bottom_row.middle_square.colour = "B"
-        self.back_face.bottom_row.right_square.colour = "B"
+        self.front_face = Face("G")
+        self.left_face = Face("R")
+        self.right_face = Face("O")
+        self.top_face = Face("Y")
+        self.bottom_face = Face("W")
+        self.back_face = Face("B")
 
     def __str__(self):
         return "FRONT\n{0}\nLEFT\n{1}\nRIGHT\n{2}\nTOP\n{3}\nBOTTOM\n{4}\nBACK\n{5}\n___________________________".format(self.front_face, self.left_face, self.right_face, self.top_face, self.bottom_face, self.back_face)
     
     def copyrow(row):
         ret = Row()
-        ret.left_square.colour = row.left_square.colour
-        ret.middle_square.colour = row.middle_square.colour
-        ret.right_square.colour = row.right_square.colour
+        ret.left_square = row.left_square
+        ret.middle_square = row.middle_square
+        ret.right_square = row.right_square
 
     def rotate(self,face):
         if "'" not in face:
@@ -122,64 +58,64 @@ class Cube:
                 f = self.right_face
                 tmp = f.bottom_row
                 # CHANGED ON OTHERS
-                col1 = self.top_face.top_row.right_square.colour
-                col2 = self.top_face.middle_row.right_square.colour
-                col3 = self.top_face.bottom_row.right_square.colour
-                self.top_face.bottom_row.right_square.colour = self.front_face.bottom_row.right_square.colour
-                self.top_face.middle_row.right_square.colour = self.front_face.middle_row.right_square.colour
-                self.top_face.top_row.right_square.colour = self.front_face.top_row.right_square.colour
+                col1 = self.top_face.top_row.right_square
+                col2 = self.top_face.middle_row.right_square
+                col3 = self.top_face.bottom_row.right_square
+                self.top_face.bottom_row.right_square = self.front_face.bottom_row.right_square
+                self.top_face.middle_row.right_square = self.front_face.middle_row.right_square
+                self.top_face.top_row.right_square = self.front_face.top_row.right_square
 
 
-                self.front_face.bottom_row.right_square.colour = self.bottom_face.top_row.right_square.colour
-                self.front_face.middle_row.right_square.colour = self.bottom_face.middle_row.right_square.colour
-                self.front_face.top_row.right_square.colour = self.bottom_face.bottom_row.right_square.colour
+                self.front_face.bottom_row.right_square = self.bottom_face.top_row.right_square
+                self.front_face.middle_row.right_square = self.bottom_face.middle_row.right_square
+                self.front_face.top_row.right_square = self.bottom_face.bottom_row.right_square
 
                 
-                self.bottom_face.bottom_row.right_square.colour = self.back_face.bottom_row.left_square.colour
-                self.bottom_face.middle_row.right_square.colour = self.back_face.middle_row.left_square.colour
-                self.bottom_face.top_row.right_square.colour = self.back_face.top_row.left_square.colour
+                self.bottom_face.bottom_row.right_square = self.back_face.bottom_row.left_square
+                self.bottom_face.middle_row.right_square = self.back_face.middle_row.left_square
+                self.bottom_face.top_row.right_square = self.back_face.top_row.left_square
 
-                self.back_face.bottom_row.left_square.colour = col1
-                self.back_face.middle_row.left_square.colour = col2
-                self.back_face.top_row.left_square.colour = col3
+                self.back_face.bottom_row.left_square = col1
+                self.back_face.middle_row.left_square = col2
+                self.back_face.top_row.left_square = col3
             elif face == "L":
                 f = self.left_face
                 tmp = f.bottom_row
                 # CHANGED ON OTHERS
-                col1 = self.top_face.top_row.left_square.colour
-                col2 = self.top_face.middle_row.left_square.colour
-                col3 = self.top_face.bottom_row.left_square.colour
-                self.top_face.bottom_row.left_square.colour = self.back_face.top_row.right_square.colour
-                self.top_face.middle_row.left_square.colour = self.back_face.middle_row.right_square.colour
-                self.top_face.top_row.right_square.colour = self.front_face.top_row.right_square.colour
+                col1 = self.top_face.top_row.left_square
+                col2 = self.top_face.middle_row.left_square
+                col3 = self.top_face.bottom_row.left_square
+                self.top_face.bottom_row.left_square = self.back_face.top_row.right_square
+                self.top_face.middle_row.left_square = self.back_face.middle_row.right_square
+                self.top_face.top_row.right_square = self.front_face.top_row.right_square
 
 
-                self.front_face.bottom_row.right_square.colour = self.bottom_face.top_row.right_square.colour
-                self.front_face.middle_row.right_square.colour = self.bottom_face.middle_row.right_square.colour
-                self.front_face.top_row.right_square.colour = self.bottom_face.bottom_row.right_square.colour
+                self.front_face.bottom_row.right_square = self.bottom_face.top_row.right_square
+                self.front_face.middle_row.right_square = self.bottom_face.middle_row.right_square
+                self.front_face.top_row.right_square = self.bottom_face.bottom_row.right_square
 
                 
-                self.bottom_face.bottom_row.right_square.colour = self.back_face.bottom_row.left_square.colour
-                self.bottom_face.middle_row.right_square.colour = self.back_face.middle_row.left_square.colour
-                self.bottom_face.top_row.right_square.colour = self.back_face.top_row.left_square.colour
+                self.bottom_face.bottom_row.right_square = self.back_face.bottom_row.left_square
+                self.bottom_face.middle_row.right_square = self.back_face.middle_row.left_square
+                self.bottom_face.top_row.right_square = self.back_face.top_row.left_square
 
-                self.back_face.bottom_row.left_square.colour = col1
-                self.back_face.middle_row.left_square.colour = col2
-                self.back_face.top_row.left_square.colour = col3
+                self.back_face.bottom_row.left_square = col1
+                self.back_face.middle_row.left_square = col2
+                self.back_face.top_row.left_square = col3
 
 
             # CHANGES ON THE ROTATING FACE 
-            f.top_row.left_square.colour = f.bottom_row.left_square.colour
-            f.top_row.middle_square.colour = f.middle_row.left_square.colour
-            f.top_row.right_square.colour = tmp.left_square.colour
+            f.top_row.left_square = f.bottom_row.left_square
+            f.top_row.middle_square = f.middle_row.left_square
+            f.top_row.right_square = tmp.left_square
 
-            f.middle_row.left_square.colour = f.bottom_row.middle_square.colour
-            f.bottom_row.left_square.colour = f.bottom_row.right_square.colour
+            f.middle_row.left_square = f.bottom_row.middle_square
+            f.bottom_row.left_square = f.bottom_row.right_square
 
-            f.bottom_row.middle_square.colour = f.middle_row.right_square.colour
-            f.bottom_row.right_square.colour = tmp.right_square.colour
+            f.bottom_row.middle_square = f.middle_row.right_square
+            f.bottom_row.right_square = tmp.right_square
 
-            f.middle_row.right_square.colour = tmp.middle_square.colour
+            f.middle_row.right_square = tmp.middle_square
         else:
             if face == "U'":
                 f = self.top_face
@@ -203,39 +139,39 @@ class Cube:
                 f = self.right_face
                 tmp = f.bottom_row
                 # CHANGED ON OTHERS
-                col1 = self.top_face.top_row.right_square.colour
-                col2 = self.top_face.middle_row.right_square.colour
-                col3 = self.top_face.bottom_row.right_square.colour
-                self.top_face.bottom_row.right_square.colour = self.back_face.top_row.left_square.colour
-                self.top_face.middle_row.right_square.colour = self.back_face.middle_row.left_square.colour
-                self.top_face.top_row.right_square.colour = self.back_face.bottom_row.left_square.colour
+                col1 = self.top_face.top_row.right_square
+                col2 = self.top_face.middle_row.right_square
+                col3 = self.top_face.bottom_row.right_square
+                self.top_face.bottom_row.right_square = self.back_face.top_row.left_square
+                self.top_face.middle_row.right_square = self.back_face.middle_row.left_square
+                self.top_face.top_row.right_square = self.back_face.bottom_row.left_square
 
-                self.back_face.bottom_row.left_square.colour = self.bottom_face.bottom_row.right_square.colour
-                self.back_face.middle_row.left_square.colour = self.bottom_face.middle_row.right_square.colour
-                self.back_face.top_row.left_square.colour = self.bottom_face.top_row.right_square.colour
+                self.back_face.bottom_row.left_square = self.bottom_face.bottom_row.right_square
+                self.back_face.middle_row.left_square = self.bottom_face.middle_row.right_square
+                self.back_face.top_row.left_square = self.bottom_face.top_row.right_square
 
-                self.bottom_face.bottom_row.right_square.colour = self.front_face.top_row.right_square.colour
-                self.bottom_face.middle_row.right_square.colour = self.front_face.middle_row.right_square.colour
-                self.bottom_face.top_row.right_square.colour = self.front_face.bottom_row.right_square.colour
+                self.bottom_face.bottom_row.right_square = self.front_face.top_row.right_square
+                self.bottom_face.middle_row.right_square = self.front_face.middle_row.right_square
+                self.bottom_face.top_row.right_square = self.front_face.bottom_row.right_square
 
 
-                self.front_face.bottom_row.right_square.colour = col3
-                self.front_face.middle_row.right_square.colour = col2
-                self.front_face.top_row.right_square.colour = col1
+                self.front_face.bottom_row.right_square = col3
+                self.front_face.middle_row.right_square = col2
+                self.front_face.top_row.right_square = col1
 
                 
             # CHANGES ON THE ROTATING FACE 
-            f.top_row.left_square.colour = tmp.right_square.colour
-            f.top_row.middle_square.colour = f.middle_row.right_square.colour
-            f.top_row.right_square.colour = f.bottom_row.right_square.colour
+            f.top_row.left_square = tmp.right_square
+            f.top_row.middle_square = f.middle_row.right_square
+            f.top_row.right_square = f.bottom_row.right_square
             
             
-            f.middle_row.right_square.colour = f.bottom_row.middle_square.colour
-            f.bottom_row.right_square.colour = f.bottom_row.left_square.colour
-            f.bottom_row.middle_square.colour = f.middle_row.left_square.colour
-            f.bottom_row.left_square.colour = tmp.left_square.colour
+            f.middle_row.right_square = f.bottom_row.middle_square
+            f.bottom_row.right_square = f.bottom_row.left_square
+            f.bottom_row.middle_square = f.middle_row.left_square
+            f.bottom_row.left_square = tmp.left_square
             
-            f.middle_row.left_square.colour = tmp.middle_square.colour
+            f.middle_row.left_square = tmp.middle_square
 
 
 
